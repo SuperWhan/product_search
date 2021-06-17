5bfa19f1f2877f2e29a01b3136a1eb9896c476cf
Zhixing Hu <kennyyhhu@hotmail.com>
2021-06-17 15:27:43 -0700

    test code

--- product_search.py
+++ product_search.py
@@ -1,4 +1,23 @@
+from bs4 import BeautifulSoup, SoupStrainer
+import re
+import requests
+from pprint import pprint
+from selenium import webdriver
+import xlsxwriter
+from openpyxl import load_workbook
+import time
 
-print('hello world')
-print('My Project')
-#123
+start_time = time.time()
+url = "http://techcrunch.com/2012/05/15/facebook-lightbox/"
+# page = requests.get(url)
+# soup = BeautifulSoup(page.content,'html.parser')
+# print(soup.prettify())
+
+browser = webdriver.Chrome()
+browser.get(url)
+html = browser.page_source
+browser.quit()
+
+soup = BeautifulSoup(html,"html.parser")
+comments = soup.findAll('div', {'class':'postText'})
+print(comments)
